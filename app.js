3aa4770bf99981a941dee39de1fd228dde4dcf35
Gabriel Richardson <gabriel.richardson.97@gmail.com>
2019-01-09 22:35:21 -0600

    Require third party modules

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log(notes.add(1,2));
+// console.log(_.isString(true));
+// console.log(_.isString('Gabriel'));
+var filteredArray = _.uniq(['Gabriel', 1, 'Gabriel', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+// console.log(notes.add(1,2));
 
 // var user = os.userInfo();
 
