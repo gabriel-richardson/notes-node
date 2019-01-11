90db902544b235c3938eefb0b0a7500a7b16ba61
Gabriel Richardson <gabriel.richardson.97@gmail.com>
2019-01-11 00:12:22 -0600

    Refactor addNote for reusability

--- notes.js
+++ notes.js
@@ -2,22 +2,32 @@ console.log('Starting notes.js');
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-    var notes = [];
+    var notes = fetchNotes();
     var note = {
         title,
         body
     };
+    var duplicateNotes = notes.filter((note) => note.title === title);
 
-    try {
-        var notesString = fs.readFileSync('notes-data.json');
-        notes = JSON.parse(notesString);
-    } catch (e) {
-        
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
     }
-
-    notes.push(note);
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
 };
 
 var removeNote = (title) => {
