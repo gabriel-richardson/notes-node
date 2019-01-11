c45c0ad45568c5c5122da8876f6e4716b6d2402c
Gabriel Richardson <gabriel.richardson.97@gmail.com>
2019-01-11 00:30:23 -0600

    Add read note functionality

--- app.js
+++ app.js
@@ -15,9 +15,7 @@ if (command === 'add') {
     var note = notes.addNote(argv.title, argv.body);
     if (note) {
         console.log('Note created');
-        console.log('--');
-        console.log(`Title: ${note.title}`);
-        console.log(`Body: ${note.body}`);
+        notes.logNote(note);
     } else {
         console.log('Note title taken');
     }
@@ -28,7 +26,13 @@ if (command === 'add') {
 } else if (command === 'list') {
     notes.getAll();
 } else if (command === 'read') {
-    notes.getNote(argv.title);
+    var note = notes.getNote(argv.title);
+    if (note) {
+        console.log('Note found');
+        notes.logNote(note);
+    } else {
+        console.log('Note not found');
+    }
 } else {
     console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -43,12 +43,21 @@ var getAll = () => {
 };
 
 var getNote = (title) => {
-    console.log('Getting note', title);
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title === title);
+    return filteredNotes[0];
+};
+
+var logNote = (note) => {
+    console.log('--');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
 };
 
 module.exports = {
     addNote,
     removeNote,
     getAll,
-    getNote
+    getNote,
+    logNote
 };
